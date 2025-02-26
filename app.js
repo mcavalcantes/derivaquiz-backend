7947f869ec54113451f53e1fd4c86ff680192669
Matheus Cavalcante <matheuscavalcantes.mc@gmail.com>
2025-02-26 17:43:13 -0300

    made changes to support multi-valued query parameters (like two types, three difficulties etc.)

--- app.js
+++ app.js
@@ -1,24 +1,64 @@
 const express = require("express");
+const cors = require("cors");
 const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 const db = new sqlite3.Database("./prisma/database.db");
 
 app.get("/api", (req, res) => {
-  const { type, difficulty } = req.query;
+  let { types, difficulties } = req.query;
 
   const response = {
     "question": "",
     "answers": [],
   };
 
-  if (!type || !difficulty) {
+  if (!types || !difficulties) {
     res.json(response);
   }
 
-  db.get('SELECT id, content FROM "Question" WHERE type = ? AND difficulty = ? ORDER BY RANDOM() LIMIT 1', [type, difficulty], (err, row) => {
+  if (!Array.isArray(types))
+    types = types ? [types] : [];
+
+  if (!Array.isArray(difficulties))
+    difficulties = difficulties ? [difficulties] : [];
+
+  let typeQuery = "";
+  switch (types.length) {
+    case 1:
+      typeQuery = `type = '${types[0]}'`;
+      break;
+    case 2:
+      typeQuery = `type = '${types[0]}' OR type = '${types[1]}'`;
+      break;
+    case 3:
+      typeQuery = `type = '${types[0]}' OR type = '${types[1]}' OR type = '${types[2]}'`;
+      break;
+  }
+
+  let difficultyQuery = "";
+  switch (difficulties.length) {
+    case 1:
+      difficultyQuery = `difficulty = '${difficulties[0]}'`;
+      break;
+    case 2:
+      difficultyQuery = `difficulty = '${difficulties[0]}' OR difficulty = '${difficulties[1]}'`;
+      break;
+    case 3:
+      difficultyQuery = `difficulty = '${difficulties[0]}' OR difficulty = '${difficulties[1]}' OR difficulty = '${difficulties[2]}'`;
+      break;
+    case 4:
+      difficultyQuery = `difficulty = '${difficulties[0]}' OR difficulty = '${difficulties[1]}' OR difficulty = '${difficulties[2]}' OR difficulty = '${difficulties[3]}'`;
+      break;
+  }
+
+  const query = `SELECT id, content FROM "Question" WHERE (${typeQuery}) AND (${difficultyQuery}) ORDER BY RANDOM() LIMIT 1`;
+
+  db.get(query, (err, row) => {
     const { id, content } = row;
     response["question"] = content;
 
